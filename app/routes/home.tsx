e10303254b0a4845ae38272b7896462a7c7ccd6b
Nagendra Mishra <nagendrasm21@gmail.com>
2025-10-19 20:53:36 +0530

    HomePage Ui Done

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyser By AI" },
+    { name: "description", content: "Welcome to Resume Analyser By AI!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-8">
+        <h1>Resume Analyser By AI</h1>
+        <p>Upload your resume and let AI analyse it for you</p>
+      </div>
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+    </section>
+  </main>;
 }

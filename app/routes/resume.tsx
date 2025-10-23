48a83acf956486cca59506b0887727caa65a21ed
Nagendra Mishra <nagendrasm21@gmail.com>
2025-10-23 13:25:19 +0530

    Wipe Data Added

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -6,11 +6,11 @@ import ATS from "~/components/ATS";
 import Details from "~/components/Details";
 
 export const meta = () => ([
-    { title: 'RABA  | Review ' },
+    { title: 'RABA | Review ' },
     { name: 'description', content: 'Detailed overview of your resume' },
 ])
 
-const resume = () => {
+const Resume = () => {
     const { auth, isLoading, fs, kv } = usePuterStore();
     const { id } = useParams();
     const [imageUrl, setImageUrl] = useState('');
@@ -49,16 +49,29 @@ const resume = () => {
         loadResume();
     }, [id]);
 
-  return (
-    <main className="!pt-0">
-        <nav className="resume-nav">
-            <Link to="/" className="back-button">
-            <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
-            <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
-            </Link>
-        </nav>
-        <div className="flex flex-row w-full max-lg:flex-col-reverse">
-            <section className="feedback-section">
+    return (
+        <main className="!pt-0">
+            <nav className="resume-nav">
+                <Link to="/" className="back-button">
+                    <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
+                    <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
+                </Link>
+            </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center w-3xl">
+                    {imageUrl && resumeUrl && (
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
+                            <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src={imageUrl}
+                                    className="w-full h-full object-contain rounded-2xl"
+                                    title="resume"
+                                />
+                            </a>
+                        </div>
+                    )}
+                </section>
+                <section className="feedback-section">
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
@@ -70,9 +83,8 @@ const resume = () => {
                         <img src="/images/resume-scan-2.gif" className="w-full" />
                     )}
                 </section>
-        </div>
-    </main>
-  )
+            </div>
+        </main>
+    )
 }
-
-export default resume
+export default Resume
